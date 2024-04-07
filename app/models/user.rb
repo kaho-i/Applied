23fe936c3bddfa1987448963566b3aa0cdb13048
EC2 Default User <ec2-user@ip-172-31-11-96.ap-northeast-1.compute.internal>
2024-04-07 12:00:56 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active, source: :followed
+  has_many :followers, through: :passive, source: :follower
+  
   has_one_attached :profile_image
 
   validates :name, length: { in: 2..20 }, uniqueness: true
@@ -14,5 +19,17 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def follow(user)
+    active.create(followed_id: user.id)
+  end
+  
+  def unfollow(user)
+    active.find_by(followed_id: user.id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
   end
 end
